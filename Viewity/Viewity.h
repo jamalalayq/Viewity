7828c75a2db313a0535d31347c46a3286e404bbb
Jamal alayq <gamalal3yk@gmail.com>
2020-01-26 13:30:27 +0200

    Initial Commit

---
+++ Viewity/Viewity.h
@@ -0,0 +1,19 @@
+//
+//  Viewity.h
+//  Viewity
+//
+//  Created by Jamal alayq on 1/26/20.
+//  Copyright © 2020 Jamal.Alayq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Viewity.
+FOUNDATION_EXPORT double ViewityVersionNumber;
+
+//! Project version string for Viewity.
+FOUNDATION_EXPORT const unsigned char ViewityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Viewity/PublicHeader.h>
+
+
